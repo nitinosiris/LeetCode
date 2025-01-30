0ea3d685135c628120eacd266a5e1867f3a8846c
nitinwartkar <nitinwartkar58@gmail.com>
2025-01-30 22:56:12 +0530

    DFS, union find

---
+++ src/CountComponents.java
@@ -0,0 +1,98 @@
+﻿import java.util.HashMap;
+import java.util.List;
+
+public class CountComponents {
+    // union find algo
+    private int[] parent;
+
+    // Find the representative (root) of the
+    // set that includes element i
+    public int find(int i) {
+
+        // if i itself is root or representative
+        if (parent[i] == i) {
+            return i;
+        }
+
+        // Else recursively find the representative
+        // of the parent
+        return find(parent[i]);
+    }
+
+    public void union(int i, int j)
+    {
+        int parent_i = find(i);
+        int parent_j = find(j);
+
+        parent[parent_i] = parent_j;
+    }
+
+
+    public int countComponents(int n, int[][] edges) {
+        // if all are disconnected
+        if(edges.length == 0)
+            return n;
+
+        parent = new int[n];
+        // Initialize the parent array with each
+        // element as its own representative
+        for(int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+
+        // add edges
+        for(int[] edge : edges)
+        {
+            union(edge[0], edge[1]);
+        }
+
+        // count how many roots are there
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+            if(parent[i] == i)
+                count++;
+
+        return count;
+
+
+//        // Create adjacency list for the undirected graph
+//        List<List<Integer>> adj = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            adj.add(new ArrayList<>());
+//        }
+//
+//        // populate the graph
+//        for(int[] edge : edges)
+//        {
+//            adj.get(edge[0]).add(edge[1]);
+//            adj.get(edge[1]).add(edge[0]);
+//        }
+//
+//        // visited array to track
+//        HashMap<Integer, Integer> visited = new HashMap<>();
+//
+//        int i = 0;
+//        int count = 0;
+//        while(visited.size() != n)
+//        {
+//            while(visited.containsKey(i))
+//                i++;
+//            dfs(adj, i, visited);
+//            count++;
+//        }
+//        return count;
+    }
+
+    private void dfs(List<List<Integer>> adj, int v, HashMap<Integer, Integer> visited)
+    {
+        visited.put(v, v);
+
+        for(int neighbor : adj.get(v))
+        {
+            if(!visited.containsKey(neighbor))
+                dfs(adj, neighbor, visited);
+        }
+    }
+}
---
+++ src/RedundentConnection.java
@@ -0,0 +1,44 @@
+﻿public class RedundentConnection {
+    int[] parent;
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length + 1;
+
+        // every element is disjoint
+        // everyone is root themselves
+        parent = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+        int[] res = new int[2];
+        for(int[] edge : edges)
+        {
+            // if root of the elements
+            // is diff, then do union or add the edge
+            if(find(edge[0]) == find(edge[1]))
+                res = edge;
+            else
+                union(edge[0], edge[1]);
+        }
+        return res;
+    }
+
+    // find the root of the given element
+    public int find(int i)
+    {
+        if(parent[i] == i)
+            return i;
+
+        return find(parent[i]);
+    }
+
+
+    // union algo
+    public void union(int i, int j)
+    {
+        int pi = find(i);
+        int pj = find(j);
+
+        parent[pi] = pj;
+    }
+}
