43699b486bc38aff018237e120c87054e5ed1621
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-04 22:40:47 +0530

    trapping water

---
+++ DailyLeetCode/src/MaxArea.java
@@ -0,0 +1,24 @@
+﻿public class MaxArea {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int maximumArea = 0;
+
+        int area = 0;
+        while(start < end)
+        {
+            if (height[start] > height[end])
+            {
+                area = (end - start) * height[end];
+                end--;
+            }
+            else
+            {
+                area = (end - start) * height[start];
+                start++;
+            }
+            maximumArea = Math.max(maximumArea, area);
+        }
+        return maximumArea;
+    }
+}
