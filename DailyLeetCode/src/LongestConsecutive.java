464983c92329b364928484c7df4ece55ced32e09
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-04 21:35:04 +0530

    Two Pointers

---
+++ DailyLeetCode/src/LongestConsecutive.java
@@ -0,0 +1,29 @@
+﻿import java.util.HashSet;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) { // Start of a sequence
+                int currNum = num;
+                int count = 1;
+
+                while (set.contains(currNum + 1)) {
+                    currNum++;
+                    count++;
+                }
+
+                maxLength = Math.max(maxLength, count);
+            }
+        }
+
+        return maxLength;
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,51 @@
-﻿import java.util.Arrays;
+﻿import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int first;
+        for(first = 0; first < nums.length; first++)
+        {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            var pairs = TwoSumList(nums, first + 1, nums.length - 1, 0 - nums[first]);
+            for (var pair : pairs) {
+                List<Integer> tripplet = new ArrayList<>();
+                tripplet.add(nums[first]);
+                tripplet.addAll(pair);
+                result.add(tripplet);
+            }
 
+        }
+        return result;
+    }
+    private List<List<Integer>> TwoSumList(int[] nums, int start, int end, int target)
+    {
+        List<List<Integer>> pairs = new ArrayList<>();
+        int i = start; int j = end;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum > target) {
+                j--; // Move the right pointer leftward to reduce the sum
+            } else if (sum < target) {
+                i++; // Move the left pointer rightward to increase the sum
+            } else {
+                // Found a valid pair
+                pairs.add(Arrays.asList(nums[i], nums[j]));
+
+                // Skip duplicates for both pointers to avoid duplicate pairs
+                while (i < j && nums[i] == nums[i + 1]) i++;
+                while (i < j && nums[j] == nums[j - 1]) j--;
+
+                // Move both pointers inward
+                i++;
+                j--;
+            }
+        }
+        return pairs;
+    }
 }
