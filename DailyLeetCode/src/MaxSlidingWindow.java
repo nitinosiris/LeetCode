b3673012872f1f0baf057beca3893795da7b00d2
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-15 13:37:07 +0530

    maxSli

---
+++ DailyLeetCode/src/MaxSlidingWindow.java
@@ -0,0 +1,28 @@
+﻿import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<AbstractMap.SimpleEntry<Integer, Integer>> pq =
+                new PriorityQueue<>((a, b) -> Integer.compare(b.getKey(), a.getKey()));
+
+        List<Integer> res = new ArrayList<>();
+        int start = 0, end = 0;
+
+        while (end < nums.length) {
+            pq.add(new AbstractMap.SimpleEntry<>(nums[end], end));
+
+            if (end - start + 1 == k) {
+                res.add(pq.peek().getKey());
+                while(!pq.isEmpty() && pq.peek().getValue() < start)
+                    pq.poll();
+                start++;
+            }
+            end++;
+        }
+
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
--- DailyLeetCode/src/Solution.java
+++ DailyLeetCode/src/Solution.java
@@ -1,2 +1,5 @@
 ﻿import java.util.*;
 
+public class Solution {
+
+}
