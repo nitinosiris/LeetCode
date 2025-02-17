3b9d206980f530fadd6a431a647c67874ff4c5cf
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-17 23:24:09 +0530

    TreeStarted

---
+++ DailyLeetCode/src/InvertBinaryTree.java
@@ -0,0 +1,32 @@
+﻿import java.util.Stack;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Use a stack to simulate the recursion
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+
+            // Swap the left and right children
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+
+            // Push children to the stack if they are not null
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+
+        return root;
+    }
+}
--- DailyLeetCode/src/Solution.java
+++ DailyLeetCode/src/Solution.java
@@ -1,5 +1,7 @@
-﻿import java.util.PriorityQueue;
-
-class Solution {
-
+﻿class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
 }
