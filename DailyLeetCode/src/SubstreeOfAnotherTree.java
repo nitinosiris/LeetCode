a9895d8360b7dbe732be6f1b372d6c13da3d411e
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-18 23:19:09 +0530

    Tree continued

---
+++ DailyLeetCode/src/SubstreeOfAnotherTree.java
@@ -0,0 +1,21 @@
+﻿public class SubstreeOfAnotherTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return false; // Only subRoot being null is not sufficient
+
+        if (isSameTree(root, subRoot))
+            return true; // Check if they are identical
+
+        // Check in left or right subtree
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        return (p.val == q.val)
+                && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
