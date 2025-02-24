c903d06e9500a3ab0d496413f52b1480c9b3f966
nitinwartkar <nitinwartkar58@gmail.com>
2025-02-25 00:11:15 +0530

    More LeetCode

---
+++ DailyLeetCode/src/NthUglyNumber.java
@@ -0,0 +1,26 @@
+﻿public class NthUglyNumber {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        int i2 = 0, i3 = 0, i5 = 0;
+        int next2 = 2, next3 = 3, next5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int nextUgly = Math.min(Math.min(next2, next3), next5);
+            dp[i] = nextUgly;
+
+            if (nextUgly == next2) {
+                next2 = dp[++i2] * 2;
+            }
+            if (nextUgly == next3) {
+                next3 = dp[++i3] * 3;
+            }
+            if (nextUgly == next5) {
+                next5 = dp[++i5] * 5;
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
---
+++ DailyLeetCode/src/TopFreqKWords.java
@@ -0,0 +1,32 @@
+﻿import java.util.*;
+
+public class TopFreqKWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        PriorityQueue<String> minHeap = new PriorityQueue<>((a, b) -> {
+            if (map.get(a) != map.get(b)) {
+                return map.get(a) - map.get(b);
+            }
+            return b.compareTo(a);
+        });
+
+        for (String word : map.keySet()) {
+            minHeap.offer(word);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        while (!minHeap.isEmpty()) {
+            ans.add(minHeap.poll());
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
