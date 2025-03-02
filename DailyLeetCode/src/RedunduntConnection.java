82db15dcbae4605e736c0743ff707fc6099e752c
nitinwartkar <nitinwartkar58@gmail.com>
2025-03-02 16:25:17 +0530

    Priority Queue

---
+++ DailyLeetCode/src/RedunduntConnection.java
@@ -0,0 +1,51 @@
+﻿public class RedunduntConnection {
+    public int[] findRedundantConnection(int[][] edges) {
+        int vertices = edges.length + 1;
+
+        int[] parent = new int[vertices];
+        int[] rank = new int[vertices];
+
+        for(int i = 0; i < vertices; i++)
+        {
+            parent[i] = i;
+        }
+
+        int[] res = new int[] {0, 0};;
+        for(var edge : edges)
+        {
+            if(find(edge[0], parent) == find(edge[1], parent))
+                res = new int[] {edge[0], edge[1]};
+
+            unionByRank(edge[0], edge[1], parent, rank);
+        }
+        return res;
+    }
+
+    private int find(int x, int[] parent)
+    {
+        int root = parent[x];
+
+        if(parent[root] != root)
+            return parent[x] = find(root, parent);
+
+        return root;
+    }
+
+    private void unionByRank(int x, int y, int[] parent, int[] rank)
+    {
+        int xRoot = find(x, parent), yRoot = find(y, parent);
+
+        if(xRoot == yRoot)
+            return;
+
+        if (rank[xRoot] < rank[yRoot])
+            parent[xRoot] = yRoot;
+        else if (rank[yRoot] < rank[xRoot])
+            parent[yRoot] = xRoot;
+        else
+        {
+            parent[yRoot] = xRoot;
+            rank[xRoot] = rank[xRoot] + 1;
+        }
+    }
+}
---
+++ DailyLeetCode/src/Twitter.java
@@ -0,0 +1,58 @@
+﻿import java.util.*;
+
+public class Twitter {
+    private static int timestamp = 0;
+
+    public class Tweet {
+        int tweetId;
+        int time;
+        public Tweet(int tweetId, int time) {
+            this.tweetId = tweetId;
+            this.time = time;
+        }
+    }
+
+    private HashMap<Integer, List<Tweet>> userMap;
+    private HashMap<Integer, HashSet<Integer>> followMap;
+
+    public Twitter() {
+        userMap = new HashMap<>();
+        followMap = new HashMap<>();
+    }
+
+    public void postTweet(int userId, int tweetId) {
+        userMap.putIfAbsent(userId, new ArrayList<>());
+        userMap.get(userId).add(new Tweet(tweetId, timestamp++));
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<Tweet> pq = new PriorityQueue<>((a, b) -> b.time - a.time);
+
+        // Get user's tweets
+        pq.addAll(userMap.getOrDefault(userId, new ArrayList<>()));
+
+        // Get followees' tweets
+        for (int followeeId : followMap.getOrDefault(userId, new HashSet<>())) {
+            pq.addAll(userMap.getOrDefault(followeeId, new ArrayList<>()));
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while (!pq.isEmpty() && res.size() < 10) {
+            res.add(pq.poll().tweetId);
+        }
+        return res;
+    }
+
+    public void follow(int followerId, int followeeId) {
+        if (followerId != followeeId) {
+            followMap.putIfAbsent(followerId, new HashSet<>());
+            followMap.get(followerId).add(followeeId);
+        }
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+        if (followMap.containsKey(followerId)) {
+            followMap.get(followerId).remove(followeeId);
+        }
+    }
+}
