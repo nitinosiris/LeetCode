16f4fcf3b3dc4b61ca8973e728c6f2ba4ba4131d
nitinwartkar <nitinwartkar58@gmail.com>
2025-03-03 23:29:42 +0530

    Matrix

---
+++ DailyLeetCode/src/SortColors.java
@@ -0,0 +1,36 @@
+﻿public class SortColors {
+    public void sortColors(int[] nums) {
+        int one = 0;
+        int two = 0;
+        int zero = 0;
+
+        for(var num : nums)
+        {
+            if(num == 0)
+                zero++;
+            else if(num == 1)
+                one++;
+            else
+                two++;
+        }
+
+        int i = 0;
+        while(zero > 0)
+        {
+            nums[i++] = 0;
+            zero--;
+        }
+
+        while(one > 0)
+        {
+            nums[i++] = 1;
+            one--;
+        }
+
+        while(two > 0)
+        {
+            nums[i++] = 2;
+            two--;
+        }
+    }
+}
---
+++ DailyLeetCode/src/SumMatrix.java
@@ -0,0 +1,29 @@
+﻿public class SumMatrix {
+    int[][] _matrix;
+    int rows, cols;
+
+    public SumMatrix(int[][] matrix) {
+        rows = matrix.length;
+        cols = matrix[0].length;
+        _matrix = new int[rows][cols];
+
+        // Compute row-wise prefix sums
+        for (int i = 0; i < rows; i++) {
+            _matrix[i][0] = matrix[i][0];
+            for (int j = 1; j < cols; j++) {
+                _matrix[i][j] = _matrix[i][j - 1] + matrix[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+
+        // Compute sum for each row in the range
+        for (int r = row1; r <= row2; r++) {
+            sum += _matrix[r][col2] - (col1 > 0 ? _matrix[r][col1 - 1] : 0);
+        }
+
+        return sum;
+    }
+}
