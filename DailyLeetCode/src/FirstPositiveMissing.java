ea43467720ece1242a3ab690a22d7e99b134d343
nitinwartkar <nitinwartkar58@gmail.com>
2025-03-04 23:37:28 +0530

    Array

---
+++ DailyLeetCode/src/FirstPositiveMissing.java
@@ -0,0 +1,32 @@
+﻿public class FirstPositiveMissing {
+    public int firstMissingPositive(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] < 0)
+                nums[i] = 0;
+        }
+
+        // positive
+        for(int i = 0; i < nums.length; i++)
+        {
+            var temp = Math.abs(nums[i]);
+            if(temp > 0 && temp <= nums.length) {
+                if(nums[temp - 1] == 0)
+                {
+                    nums[temp - 1] = -temp;
+                }
+                else if (nums[temp - 1] > 0)
+                {
+                    nums[temp - 1] = -1 * nums[temp - 1];
+                }
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] >= 0)
+                return i + 1;
+        }
+        return nums.length + 1;
+    }
+}
