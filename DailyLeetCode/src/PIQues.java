5888233a6d1adc658b86ad2591cac02e0d61f5a9
nitinwartkar <nitinwartkar58@gmail.com>
2025-05-19 00:37:46 +0530

    Google coding prep daY 5

---
+++ DailyLeetCode/src/PIQues.java
@@ -0,0 +1,26 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class PIQues {
+    public List<Integer> check(String pi) {
+        List<Integer> ans = new ArrayList<>();
+
+        int n = pi.length();
+        for (int i = 1; i <= n; i++) {
+            String numStr = String.valueOf(i);
+            int len = numStr.length();
+
+            if (i < len || i > n)
+                continue;
+
+            int startIdx = i - len;
+            String piSub = pi.substring(startIdx, i);
+
+            if (piSub.equals(numStr)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
